b4432a3fb6a6cf6acf51f876175960d0703054d1
tiagonogueira2006 <silvahenriquetiagodragon@gmail.com>
2025-08-28 20:53:28 -0300

    Atividade 4

---
+++ Atidades.Java/src/Atividade04.java
@@ -0,0 +1,11 @@
+public class Atividade04 {
+    public static void main(String[] args) {
+        final double PI = 3.1415;
+        double raio = 0;
+
+        raio = 2;
+
+        System.out.println("PI = " + PI + "\n" +
+                "Raio = " + raio);
+    }
+}
