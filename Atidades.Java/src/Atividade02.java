46ccdaca30acf389bd25717768cce17b764c2000
tiagonogueira2006 <silvahenriquetiagodragon@gmail.com>
2025-08-28 19:45:29 -0300

    Atividade 2

---
+++ Atidades.Java/src/Atividade02.java
@@ -0,0 +1,13 @@
+
+public class Atividade02 {
+    public static void main(String[] args) {
+        String dia = "29";
+        String mes = "06";
+        String ano = "2006";
+
+        System.out.println(dia);
+        System.out.println(mes);
+        System.out.println(ano);
+
+    }
+}
