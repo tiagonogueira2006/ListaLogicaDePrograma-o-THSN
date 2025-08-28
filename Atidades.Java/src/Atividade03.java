09ae331f8a301ec7a90502d0dc0ecaadbbfdfef6
tiagonogueira2006 <silvahenriquetiagodragon@gmail.com>
2025-08-28 19:50:36 -0300

    Atividade 3

---
+++ Atidades.Java/src/Atividade03.java
@@ -0,0 +1,13 @@
+public class Atividade03 {
+    public static void main(String[] args) {
+        int qtdAlunos = 100;
+        double mediaTurma = 100.0;
+        boolean aprovada = true;
+        String professor = "Vitor";
+
+        System.out.println("Quantidade de alunos = " + qtdAlunos + "\n"+
+                "Média da turma = " + mediaTurma + "\n"+
+                "Aprovados = " + aprovada + "\n"+
+                "Professor = " + professor + "\n");
+    }
+}
