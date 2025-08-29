f23c4baa3e75d0b92aacff6b2b0704946e2a36a2
tiagonogueira2006 <silvahenriquetiagodragon@gmail.com>
2025-08-28 21:04:22 -0300

    Atividade 6

---
+++ Atidades.Java/src/Atividade06.java
@@ -0,0 +1,9 @@
+
+public class Atividade06 {
+    public static void main(String[] args) {
+        int idade = 19;
+        final int meses = 12;
+
+        System.out.println("A sua idade em meeses é: " + idade*meses);
+    }
+}
