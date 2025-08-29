7d23a63789d1ea88172ac9a69503d5d4900ca1c4
tiagonogueira2006 <silvahenriquetiagodragon@gmail.com>
2025-08-28 21:00:55 -0300

    Atividade 5

---
+++ Atidades.Java/src/Atividade05.java
@@ -0,0 +1,9 @@
+public class Atividade05 {
+    public static void main(String[] args) {
+        String curso = "Como vender curso";
+        int cargaHor = 1000000;
+
+        System.out.println("Curso: " + curso + "\n" + "Carga horária: " + cargaHor +"h");
+
+    }
+}
